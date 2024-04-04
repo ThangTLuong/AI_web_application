2613fd79fd30596d10f0f2c8961b672d3853d08e
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-04-04 15:01:18 -0700

    Added Submit button

--- application/src/app/( pages )/neuro/page.tsx
+++ application/src/app/( pages )/neuro/page.tsx
@@ -1,10 +1,20 @@
 import "@/styles/neuro.css";
 
 export default function Neuro() {
+  async function onSubmitButton(formData: FormData) {
+    "use server";
+    let message: string = formData.get("name") as string;
+    message = message.trim();
+    console.log(message);
+  }
+
   return (
     <div className="page-container">
       <div className="chat-history"></div>
-      <textarea className="user-input" placeholder="Message" />
+      <form action={onSubmitButton}>
+        <textarea name="name" className="user-input" placeholder="Message" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
