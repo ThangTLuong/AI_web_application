99b3532516ec7767aa16aee7e45662a5e8326b06
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-04-05 11:27:14 -0700

    Neuro page style

--- application/src/app/( pages )/neuro/page.tsx
+++ application/src/app/( pages )/neuro/page.tsx
@@ -1,19 +1,21 @@
 import "@/styles/neuro.css";
 
 export default function Neuro() {
-  async function onSubmitButton(formData: FormData) {
+  async function onSubmit(formData: FormData) {
     "use server";
     let message: string = formData.get("name") as string;
     message = message.trim();
-    console.log(message);
+    if (message !== "") console.log(message);
   }
 
   return (
     <div className="page-container">
       <div className="chat-history"></div>
-      <form action={onSubmitButton}>
+      <form className="form" action={onSubmit}>
         <textarea name="name" className="user-input" placeholder="Message" />
-        <button type="submit">Submit</button>
+        <button type="submit">
+          <div className="submit-button"></div>
+        </button>
       </form>
     </div>
   );
