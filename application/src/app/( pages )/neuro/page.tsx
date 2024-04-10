81de4a4d9e930bfd4a14765c29746fcea4b659e1
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-04-10 13:10:40 -0700

    use server and client component error

--- application/src/app/( pages )/neuro/page.tsx
+++ application/src/app/( pages )/neuro/page.tsx
@@ -1,16 +1,27 @@
 import "@/styles/neuro.css";
+import React from "react";
 
 export default function Neuro() {
+  const chatHistory: string[] = [];
+
   async function onSubmit(formData: FormData) {
     "use server";
     let message: string = formData.get("name") as string;
     message = message.trim();
-    if (message !== "") console.log(message);
+    if (message !== "") chatHistory.push(message);
+  }
+
+  async function onEnter(event: React.KeyboardEvent<HTMLTextAreaElement>) {
+    alert("It worked?");
   }
 
   return (
     <div className="page-container">
-      <div className="chat-history"></div>
+      <div className="chat-history">
+        {chatHistory.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </div>
       <form className="form" action={onSubmit}>
         <textarea name="name" className="user-input" placeholder="Message" />
         <button type="submit">
