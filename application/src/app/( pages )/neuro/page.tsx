5a429711fabbeea9f2a13830fdc8dd881ed84579
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2024-04-10 13:40:13 -0700

    Fixed crashes

--- application/src/app/( pages )/neuro/page.tsx
+++ application/src/app/( pages )/neuro/page.tsx
@@ -1,29 +1,43 @@
+"use client";
+
 import "@/styles/neuro.css";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Neuro() {
-  const chatHistory: string[] = [];
+  const [chatHistory, setChatHistory] = useState<string[]>([]);
 
   async function onSubmit(formData: FormData) {
-    "use server";
     let message: string = formData.get("name") as string;
-    message = message.trim();
-    if (message !== "") chatHistory.push(message);
+    appendChatHistory(message);
   }
 
   async function onEnter(event: React.KeyboardEvent<HTMLTextAreaElement>) {
-    alert("It worked?");
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      appendChatHistory(event.currentTarget.value as string);
+    }
+  }
+
+  async function appendChatHistory(entry: string) {
+    let message: string = entry.trim();
+    if (message !== "")
+      setChatHistory((prevHistory) => [...prevHistory, message]);
   }
 
   return (
     <div className="page-container">
       <div className="chat-history">
-        {chatHistory.map((message, index) => (
+        {chatHistory?.map((message, index) => (
           <p key={index}>{message}</p>
         ))}
       </div>
       <form className="form" action={onSubmit}>
-        <textarea name="name" className="user-input" placeholder="Message" />
+        <textarea
+          name="name"
+          className="user-input"
+          placeholder="Message"
+          onKeyDown={onEnter}
+        />
         <button type="submit">
           <div className="submit-button"></div>
         </button>
